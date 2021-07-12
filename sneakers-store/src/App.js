ed6952a93b4d1f766b898628b6aa356f4a921d3b
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-07-12 20:43:01 +0200

    added components

--- sneakers-store/src/App.js
+++ sneakers-store/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Sneakers Store</h1>
+      <h1 className="text text-cyan-800" >Sneakers Store</h1>
     </div>
   );
 }
